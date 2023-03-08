b0b4bffa04596a42776b82330c42f1c097195dfb
andrewtrz <120880637+andrewtrz@users.noreply.github.com>
2023-03-08 15:44:20 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,36 +3,30 @@ import altair as alt
 import math
 import pandas as pd
 import streamlit as st
+import streamlit as st
+import pandas as pd
+import plotly.express as px
 
 """
-# Welcome to Streamlit!
-
-Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:
-
-If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
-forums](https://discuss.streamlit.io).
-
-In the meantime, below is an example of what you can do with just a few lines of code:
+R+D Spend in the USA
 """
 
+# Load R&D spending data
+rd_data = pd.read_csv('path/to/rd_data.csv')
 
-with st.echo(code_location='below'):
-    total_points = st.slider("Number of points in spiral", 1, 5000, 2000)
-    num_turns = st.slider("Number of turns in spiral", 1, 100, 9)
+# Create sidebar widgets
+year_range = st.sidebar.slider('Select a year range', min_value=rd_data['Year'].min(), max_value=rd_data['Year'].max(), value=(rd_data['Year'].min(), rd_data['Year'].max()))
+sector = st.sidebar.selectbox('Select a sector', ['Total', 'Government', 'Business', 'Higher Education', 'Nonprofit'])
 
-    Point = namedtuple('Point', 'x y')
-    data = []
+# Filter data based on sidebar widgets
+rd_data_filtered = rd_data[(rd_data['Year'] >= year_range[0]) & (rd_data['Year'] <= year_range[1])]
+if sector != 'Total':
+    rd_data_filtered = rd_data_filtered[rd_data_filtered['Sector'] == sector]
 
-    points_per_turn = total_points / num_turns
+# Create plot
+fig = px.line(rd_data_filtered, x='Year', y='R&D', color='Sector')
 
-    for curr_point_num in range(total_points):
-        curr_turn, i = divmod(curr_point_num, points_per_turn)
-        angle = (curr_turn + 1) * 2 * math.pi * i / points_per_turn
-        radius = curr_point_num / total_points
-        x = radius * math.cos(angle)
-        y = radius * math.sin(angle)
-        data.append(Point(x, y))
+# Display plot and title
+st.plotly_chart(fig)
+st.title('R&D Spending in the USA')
 
-    st.altair_chart(alt.Chart(pd.DataFrame(data), height=500, width=500)
-        .mark_circle(color='#0068c9', opacity=0.5)
-        .encode(x='x:Q', y='y:Q'))
